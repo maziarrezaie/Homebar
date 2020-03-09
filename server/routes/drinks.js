e7dd403c4a84f64196bdbe2396b1383cc7ebeb86
maziar <maziar.rezaiee@gmail.com>
2020-03-09 14:26:02 +0100

    first commit

---
+++ server/routes/drinks.js
@@ -0,0 +1,51 @@
+const router = require('express').Router();
+const MongoClient = require('mongodb').MongoClient;
+const url = `mongodb+srv://maziar:maziar123@cluster0-qxuag.mongodb.net/test?retryWrites=true&w=majority`
+
+
+router.get("/filter", (req, res, next)=>{
+MongoClient.connect(url,{useUnifiedTopology: true}, (error, con )=> {
+    if (error) throw error ;
+ const dbo = con.db("likor");
+ 
+ //To Insert into database
+ /* dbo.collection("drinks").insertOne({base: "gin"}, (err, result)=>{
+     if(err) throw err;
+     console.log(result);
+     con.close()
+ }) */
+
+ const answers = req.body;
+
+  dbo.collection("drinks").find(answers).toArray((err, result)=>{
+     if(err) throw err;
+     console.log(result);
+     res.send(result)
+     con.close();
+ }); 
+})
+
+
+});
+
+router.post('/add', function(req, res) {
+    console.log(req.body);
+    MongoClient.connect(url,{useUnifiedTopology: true}, (error, con )=> {
+        if (error) throw error ;
+     const dbo = con.db("likor");
+     
+     //To Insert into database
+      dbo.collection("drinks").insertOne(req.body, (err, result)=>{
+         if(err) throw err;
+         console.log(result);
+         con.close()
+         res.send({
+            msg: "drink successfully inserted",
+            result:result
+        })
+      });
+      });
+      
+  });
+
+module.exports = router;
---
+++ server/routes/users.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var router = express.Router();
+const MongoClient = require('mongodb').MongoClient;
+const url = `mongodb+srv://maziar:maziar123@cluster0-qxuag.mongodb.net/test?retryWrites=true&w=majority`
+
+
+
+/* Register User. */
+router.post('/register', function(req, res) {
+  MongoClient.connect(url,{useUnifiedTopology: true}, (error, con )=> {
+    if (error) throw error ;
+    const dbo = con.db("likor");
+    const user = req.body;
+
+    dbo.collection("users").insertOne(user, (err, result)=>{
+      if(err) throw err;
+      res.send("User Registered Successfully")
+      con.close()
+    });
+});
+
+});
+
+
+router.post('/add', function(req, res) {
+  console.log(req.body);
+});
+
+
+module.exports = router;
